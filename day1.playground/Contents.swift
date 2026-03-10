59f1cf0a80be42f26dec8a6755f84539ac3d6bd3
mama0048 <mama0048@uni.sydney.edu.au>
2026-03-10 12:53:24 +1100

    lesson: variables and constants

---
+++ day1.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Cocoa // lots of functionality
+
+var greeting = "Hello, playground" // create a new variable
+greeting = "Hi, playground" // can change
+
+let character = "Mia" // constant, can't change
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+let importantWealth = "love"

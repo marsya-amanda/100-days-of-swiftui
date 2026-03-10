522a7186bd58087b74efa8a14220489279356dad
mama0048 <mama0048@uni.sydney.edu.au>
2026-03-10 13:00:03 +1100

    lesson: strings

--- day1.playground/Contents.swift
+++ day1.playground/Contents.swift
@@ -12,3 +12,19 @@ playerName = "Dani"
 print(playerName)
 
 let importantWealth = "love"
+
+// Creating strings!
+let quote = "\"hello\"" // backslashes before quotes
+print(quote)
+
+let multilineQuote = """
+A day in
+the life of an
+Apple engineer
+"""
+
+print(quote.count) // count is a property, just read
+print(quote.uppercased()) // this is an operation
+print(multilineQuote.hasPrefix("A day"))
+print(multilineQuote.hasSuffix("engineer"))
+print(multilineQuote.hasSuffix("Engineer")) // false! Case-sensitive

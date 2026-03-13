e549d6b6b2c0b7c3a51c0b8e88b87ced4aa4813f
mama0048 <mama0048@uni.sydney.edu.au>
2026-03-13 15:36:01 +1100

    day4

--- day4.playground/Contents.swift
+++ day4.playground/Contents.swift
@@ -1,3 +1,21 @@
 import Cocoa
+// https://www.hackingwithswift.com/100/swiftui/4
 
-var greeting = "Hello, playground"
+// # DAY 4: Complex data types, part 2
+
+// // Type annotations
+
+// practice: try to use type inference as much as possible, ie. let Swift decide
+let favouriteNumber: Int = 2
+var albums: [String] = ["CTRL"]
+var user: [String: String] = ["username": "marsya-amanda"]
+var books: Set<String> = Set(["One Hundred Years of Solitude", "The Handmaid's Tale"])
+
+// initialising without value
+let username: String // type annotation REQUIRED
+username = "@marsya-amanda"
+
+// // CHECKPOINT 2
+let arr = ["mars", "miu", "declan", "emerson", "lilly", "mars", "mars", "declan"]
+print(arr.count) // prints number of items in array
+print(Set(arr).count) // prints number of unique items in array

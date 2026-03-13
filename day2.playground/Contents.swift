2b4031424172eedb1bc4cffad2b9268560a815a9
mama0048 <mama0048@uni.sydney.edu.au>
2026-03-13 13:26:33 +1100

    day2: boolean

---
+++ day2.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Cocoa
+// https://www.hackingwithswift.com/100/swiftui/2
+
+// # Booleans, String interpolation, Checkpoint 1
+
+// ## Bool
+let gameOver = false
+let isMultiple: Bool = 120.isMultiple(of: 3)
+
+// // operator
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+
+// // functionality
+gameOver.toggle() // WOW toggle value
+
